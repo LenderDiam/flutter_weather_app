2da3b291d0d6d2de08f8fb06a42aae5d77fe46af
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-06-14 14:41:06 +0200

    Create theme.dart

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'custom_themes/color_scheme.dart';
+import 'custom_themes/text_theme.dart';
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/card_theme.dart';
+import 'custom_themes/button_theme.dart';
+import 'custom_themes/input_theme.dart';
+
+class AppTheme {
+  static ThemeData light = ThemeData(
+    useMaterial3: true,
+    colorScheme: AppColors.light,
+    textTheme: AppTextTheme.light,
+    appBarTheme: appBarLightTheme,
+    cardTheme: appCardLightTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: elevatedButtonStyleLight),
+    inputDecorationTheme: inputLightTheme,
+    scaffoldBackgroundColor: AppColors.backgroundLight,
+  );
+
+  static ThemeData dark = ThemeData(
+    useMaterial3: true,
+    colorScheme: AppColors.dark,
+    textTheme: AppTextTheme.dark,
+    appBarTheme: appBarDarkTheme,
+    cardTheme: appCardDarkTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: elevatedButtonStyleDark),
+    inputDecorationTheme: inputDarkTheme,
+    scaffoldBackgroundColor: AppColors.backgroundDark,
+  );
+}
