2a58b3010bb1c5d144aa9d1d5d7d2f9b9d7f9024
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-06-18 16:58:23 +0200

    Update card_theme.dart

--- lib/utils/theme/custom_themes/card_theme.dart
+++ lib/utils/theme/custom_themes/card_theme.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'color_scheme.dart';
 
 final CardTheme appCardLightTheme = CardTheme(
-  color: Colors.white,
+  color: AppColors.light.surface,
   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
   margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
 );
 
 final CardTheme appCardDarkTheme = CardTheme(
-  color: AppColors.pictonBlue.withOpacity(0.1),
+  color: AppColors.dark.surface.withOpacity(0.1),
   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
   margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
 );
